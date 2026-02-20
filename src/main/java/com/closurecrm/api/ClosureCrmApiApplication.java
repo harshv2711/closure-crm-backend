76e6793d8e0297701df5ba5e6ed7e3579e553143
harsh <harsh@pivotmkg.com>
2026-02-20 23:35:27 +0530

    first commit

---
+++ src/main/java/com/closurecrm/api/ClosureCrmApiApplication.java
@@ -0,0 +1,13 @@
+package com.closurecrm.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClosureCrmApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClosureCrmApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/closurecrm/api/ClosureCrmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.closurecrm.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClosureCrmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
